1f1df60c7568ddb1866545ea6c5f808b8c77a435
Dorotea <35565194+Dorole@users.noreply.github.com>
2021-10-23 19:32:22 +0200

    Basic player movement
    The player can move on input (Space) between 1 and 6 spaces on the board. Once the player reaches 100th tile/node, it can't move further.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Route _route;
+
+    private List<Node> _nodeList = new List<Node>();
+    private int _playerPosition;
+
+    [SerializeField]
+    private float _speed = 8.0f;
+    [SerializeField]
+    private float _waitBetweenNodes = 0.1f;
+
+    private int _stepsToMove;
+    private int _doneSteps;
+
+    private bool _isMoving;
+
+    private void Start()
+    {
+        foreach (Transform node in _route.nodeList)
+        {
+            Node n = node.GetComponent<Node>();
+
+            if (n != null)
+                _nodeList.Add(n);
+        }
+    }
+
+    void Update()
+    {
+        //debug, delete later
+        if (Input.GetKeyDown(KeyCode.Space) && !_isMoving)
+        {
+            _stepsToMove = Random.Range(1, 7); //dice simulation
+            Debug.Log("Rolled: " + _stepsToMove);
+
+            if (_doneSteps + _stepsToMove < _route.nodeList.Count)
+                StartCoroutine(Move());
+            else
+                Debug.Log("The rolled number is too high!");
+        }
+    }
+
+    private IEnumerator Move()
+    {
+        if (_isMoving)
+            yield break;
+
+        _isMoving = true;
+
+        while (_stepsToMove > 0)
+        {
+            _playerPosition++;
+            Vector3 nextPosition = _route.nodeList[_playerPosition].transform.position;
+            nextPosition.z = -0.05f; //offset - otherwise the player is hidden behind the node 
+
+            while(MoveToNextNode(nextPosition))
+            {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(_waitBetweenNodes);
+
+            _stepsToMove--;
+            _doneSteps++;
+        }
+
+        _isMoving = false;
+    }
+
+    private bool MoveToNextNode(Vector3 nextPos)
+    {
+        return nextPos != (transform.position = Vector3.MoveTowards(transform.position, nextPos, _speed * Time.deltaTime));
+    }
+
+
+}
